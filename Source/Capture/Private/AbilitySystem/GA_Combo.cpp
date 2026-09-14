e3728a083047ef865200d8b24cb4f8bdcd9494d7
raka_07 <raka3dprogramming@gmail.com>
2026-09-14 17:10:56 -0500

    AnimMontage added

--- Source/Capture/Private/AbilitySystem/GA_Combo.cpp
+++ Source/Capture/Private/AbilitySystem/GA_Combo.cpp
@@ -2,10 +2,17 @@
 
 
 #include "AbilitySystem/GA_Combo.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "AbilitySystem/CAbilitySystemNativeTags.h"
 
+UGA_Combo::UGA_Combo()
+{
+	AbilityTags.AddTag(TAG_ABILITY_BASICATTACK);
+	BlockAbilitiesWithTag.AddTag(TAG_ABILITY_BASICATTACK);
+}
 
 void UGA_Combo::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
-	const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+                                const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 	
@@ -15,4 +22,15 @@ void UGA_Combo::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const F
 		return;
 	}
 	UE_LOG(LogTemp,Warning,TEXT("Casting combo ability"))
+	
+	if (HasAuthorityOrPredictionKey(ActorInfo, &ActivationInfo))
+	{
+		UAbilityTask_PlayMontageAndWait* PlayMontageAndWaitTask = 
+			UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, NAME_None,ComboMontage);
+		PlayMontageAndWaitTask-> OnCompleted.AddDynamic(this,&UGA_Combo::K2_EndAbility);
+		PlayMontageAndWaitTask-> OnCancelled.AddDynamic(this,&UGA_Combo::K2_EndAbility);
+		PlayMontageAndWaitTask-> OnInterrupted.AddDynamic(this,&UGA_Combo::K2_EndAbility);
+		PlayMontageAndWaitTask-> OnBlendOut.AddDynamic(this,&UGA_Combo::K2_EndAbility);
+		PlayMontageAndWaitTask->ReadyForActivation();
+	}
 }
--- Source/Capture/Private/AbilitySystem/GA_Combo.h
+++ Source/Capture/Private/AbilitySystem/GA_Combo.h
@@ -13,12 +13,15 @@ UCLASS()
 class UGA_Combo : public UCGameplayAbility
 {
 	GENERATED_BODY()
-	
+	UGA_Combo();
 public:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, 
 		const FGameplayAbilityActorInfo* ActorInfo, 
 		const FGameplayAbilityActivationInfo ActivationInfo, 
 		const FGameplayEventData* TriggerEventData) override;
 	
+private:
+	UPROPERTY(EditDefaultsOnly, category = "Animation")
+	UAnimMontage* ComboMontage;
 
 };
