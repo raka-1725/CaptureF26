986b031b47c389bb51e70b64cf13397a3f98b1a5
raka_07 <raka3dprogramming@gmail.com>
2026-09-14 17:45:34 -0500

    Anim Montage set up

---
+++ Source/Capture/Private/AbilitySystem/CAbilitySystemNativeTags.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/CAbilitySystemNativeTags.h"
+
+//defination
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_ABILITY_BASICATTACK, "ability.basicattack", "tag given to the basic attack ability")
---
+++ Source/Capture/Private/AbilitySystem/CAbilitySystemNativeTags.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NativegameplayTags.h"
+
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_ABILITY_BASICATTACK);
+
+//(Extern) is decleartion only
+extern int TestTest = 0;
+
+
+/**
+ * 
+ */
+class CAbilitySystemNativeTags
+{
+public:
+	CAbilitySystemNativeTags();
+	~CAbilitySystemNativeTags();
+};
